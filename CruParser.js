5a41d3082fb4ee8e2918c6b664bffe92dc8922b8
Valentin <v.bocquier@outlook.fr>
2024-11-27 17:58:34 +0100

    Adaptation du Parser pour le formatCRU

---
+++ CruParser.js
@@ -0,0 +1,77 @@
+const EDT = require("./edt"); // Assurez-vous d'importer EDT ici
+
+var CruParser = function (sTokenize, sParsedSymb) {
+  this.parsedData = []; // Liste des sessions analysées
+  this.showTokenize = sTokenize;
+  this.showParsedSymbols = sParsedSymb;
+  this.errorCount = 0;
+};
+
+// Tokenize : transforme les données d'entrée en liste
+CruParser.prototype.tokenize = function (data) {
+  // Diviser les données en lignes
+  var separator = /(\r\n|: )/;
+  data = data.split(separator);
+  data = data.filter((val, idx) => !val.match(separator));
+
+  filteredData = data.filter((line) => {
+    // Ignore les lignes parasites
+    return (
+      line.trim() !== "" &&
+      !line.includes("Page") &&
+      !line.startsWith("+UVUV") &&
+      !line.startsWith("Seance") &&
+      !line.endsWith("sec")
+    );
+  });
+  return filteredData;
+};
+
+// Parse : analyse les données en appelant la première règle non terminale
+CruParser.prototype.parse = function (data) {
+  var tData = this.tokenize(data);
+  if (this.showTokenize) {
+    console.log(tData);
+  }
+  this.listEdt(tData);
+};
+
+// Analyse la liste des sessions
+CruParser.prototype.listEdt = function (input) {
+  var currentEDT;
+  while (input.length > 0) {
+    var line = input.shift();
+
+    if (line.startsWith("+")) {
+      // Si c'est un nouvel EDT (ligne commence par "+")
+      if (currentEDT) {
+        this.parsedData.push(currentEDT); // Ajouter l'edt précédent à la liste
+      }
+      // Commencer une nouvelle session
+      currentEDT = new EDT(line.substring(1), []); // Créer une nouvelle instance EDT
+    } else {
+      // Sinon, analyser les activités de l'EDT courant
+      if (currentEDT) {
+        currentEDT.sessions.push(this.session(line)); // Ajouter la session à la liste de session
+      }
+    }
+  }
+  if (currentEDT) {
+    this.parsedData.push(currentEDT);
+  }
+};
+
+// Analyse une activité dans le format "X,X,P=X,H=X X:XX-X:XX,XX,S=XXX//"
+CruParser.prototype.session = function (line) {
+  var parts = line.split(",");
+  var id = parts[0]; // ID de l'activité
+  var group = parts[1]; // Groupe
+  var participants = parts[2].split("=")[1]; // Nombre de participants
+  var time = parts[3].split("=")[1]; // Jour Horaire
+  var subgroup = parts[4]; // Sous-groupe
+  var room = parts[5].split("=")[1].replace("//", ""); // Salle
+
+  return [id, group, participants, time, subgroup, room];
+};
+
+module.exports = CruParser;
